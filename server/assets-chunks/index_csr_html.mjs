9add99c2da2ada94a7a5ead88a690a9349d02258
Irving Abimael Montes Zapata <irving.monteszapata@cesunbc.edu.mx>
2025-12-12 14:06:07 -0800

    Auto-generated commit

---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>CateringLanding</title>
+  <base href="C:/Program Files/Git/catering-landing/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<style>:root{--bg:#0b0d12;--card:#121622;--text:#e9ecf1;--muted:#b6bdc9;--brand:#ffb703;--brand2:#fb8500;--border:rgba(255,255,255,.1)}*{box-sizing:border-box}html{scroll-behavior:smooth}body{margin:0;background:var(--bg);color:var(--text);font-family:system-ui,-apple-system,Segoe UI,Roboto,Arial,sans-serif}</style><link rel="stylesheet" href="styles-X4GQPHCK.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-X4GQPHCK.css"></noscript></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="main-Y4FR64P2.js" type="module"></script></body>
+</html>
+`;
